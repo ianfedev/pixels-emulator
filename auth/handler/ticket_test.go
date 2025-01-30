2ae548d393fcdced103c4b1bd47c2d77cd3f9140
Ian Castaño <ianfe@martina.com.co>
2025-01-30 16:20:21 -0500

    partial: Keep adding boilerplate testings

--- auth/handler/ticket_test.go
+++ auth/handler/ticket_test.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 	grant "pixels-emulator/auth/event"
@@ -73,5 +75,154 @@ func TestAuthTicketHandler_Handle(t *testing.T) {
 	ssoSvc.AssertExpectations(t)
 	userSvc.AssertExpectations(t)
 	em.AssertExpectations(t)
+
+}
+
+func TestAuthTicketHandler_HandleDebug(t *testing.T) {
+
+	log, buf := util.CreateTestLogger()
+	demoId := "1"
+	idNum, _ := strconv.Atoi(demoId)
+
+	cfg := &config.Config{
+		Server: config.ServerConfig{
+			Environment: "DEVELOPMENT",
+		},
+	}
+
+	con := &mockproto.MockConnection{}
+	con.On("GrantIdentifier", mock.Anything).Return(nil)
+	con.On("Identifier").Return(demoId)
+
+	em := &mockevent.MockEventManager{}
+	em.On("Fire", grant.AuthGrantEventName, mock.Anything).Return(nil)
+
+	ssoSvc := &mockdb.ModelServiceMock[model.SSOTicket]{}
+
+	userSvc := &mockdb.ModelServiceMock[model.User]{}
+	userSvc.On("Get", mock.MatchedBy(func(id uint) bool {
+		return id == uint(idNum)
+	})).Return(util.MockAsyncResponse(&model.User{BaseModel: database.BaseModel{ID: uint(idNum)}}, nil))
+
+	pck := &message.AuthTicketPacket{
+		Ticket: demoId,
+		Time:   1,
+	}
+
+	handler := setupHandler(log, em, ssoSvc, userSvc, cfg)
+
+	handler.Handle(pck, con)
+	ssoSvc.AssertExpectations(t)
+	userSvc.AssertExpectations(t)
+	em.AssertExpectations(t)
+	assert.Contains(t, buf.String(), "Attempting to log in debug mode, SSO ticket will be taken as user id, switch to production to prevent this")
+
+}
+
+func TestAuthTicketHandler_QueryError(t *testing.T) {
+
+	log, buf := util.CreateTestLogger()
+	demoId := "1"
+
+	cfg := &config.Config{
+		Server: config.ServerConfig{
+			Environment: "PRODUCTION",
+		},
+	}
+
+	con := &mockproto.MockConnection{}
+	con.On("Dispose").Return(nil)
+
+	em := &mockevent.MockEventManager{}
+
+	ssoSvc := &mockdb.ModelServiceMock[model.SSOTicket]{}
+	ssoSvc.On("FindByQuery", mock.MatchedBy(func(q map[string]interface{}) bool {
+		return q["ticket"] == demoId
+	})).Return(util.MockAsyncResponse([]model.SSOTicket{}, errors.New("query error")))
+
+	userSvc := &mockdb.ModelServiceMock[model.User]{}
+
+	pck := &message.AuthTicketPacket{
+		Ticket: demoId,
+		Time:   1,
+	}
+
+	handler := setupHandler(log, em, ssoSvc, userSvc, cfg)
+
+	handler.Handle(pck, con)
+	ssoSvc.AssertExpectations(t)
+	assert.Contains(t, buf.String(), "query error")
+
+}
+
+func TestAuthTicketHandler_DuplicatedSession(t *testing.T) {
+
+	log, buf := util.CreateTestLogger()
+	demoId := "1"
+
+	cfg := &config.Config{
+		Server: config.ServerConfig{
+			Environment: "PRODUCTION",
+		},
+	}
+
+	con := &mockproto.MockConnection{}
+	con.On("Dispose").Return(nil)
+
+	em := &mockevent.MockEventManager{}
+
+	ssoSvc := &mockdb.ModelServiceMock[model.SSOTicket]{}
+	ssoSvc.On("FindByQuery", mock.MatchedBy(func(q map[string]interface{}) bool {
+		return q["ticket"] == demoId
+	})).Return(util.MockAsyncResponse([]model.SSOTicket{}, nil))
+
+	userSvc := &mockdb.ModelServiceMock[model.User]{}
+
+	pck := &message.AuthTicketPacket{
+		Ticket: demoId,
+		Time:   1,
+	}
+
+	handler := setupHandler(log, em, ssoSvc, userSvc, cfg)
+
+	handler.Handle(pck, con)
+	ssoSvc.AssertExpectations(t)
+	assert.Contains(t, buf.String(), "session is being duplicated")
+
+}
+
+func TestAuthTicketHandler_InvalidSession(t *testing.T) {
+
+	log, buf := util.CreateTestLogger()
+	demoId := "1"
+
+	cfg := &config.Config{
+		Server: config.ServerConfig{
+			Environment: "PRODUCTION",
+		},
+	}
+
+	con := &mockproto.MockConnection{}
+	con.On("Dispose").Return(nil)
+
+	em := &mockevent.MockEventManager{}
+
+	ssoSvc := &mockdb.ModelServiceMock[model.SSOTicket]{}
+	ssoSvc.On("FindByQuery", mock.MatchedBy(func(q map[string]interface{}) bool {
+		return q["ticket"] == demoId
+	})).Return(util.MockAsyncResponse([]model.SSOTicket{}, nil))
+
+	userSvc := &mockdb.ModelServiceMock[model.User]{}
+
+	pck := &message.AuthTicketPacket{
+		Ticket: demoId,
+		Time:   1,
+	}
+
+	handler := setupHandler(log, em, ssoSvc, userSvc, cfg)
+
+	handler.Handle(pck, con)
+	ssoSvc.AssertExpectations(t)
+	assert.Contains(t, buf.String(), "session is being created with not valid ticket")
 
 }
