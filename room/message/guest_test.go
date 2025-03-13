61050df53c55dca753a49f3b2a643d26d66384d1
Ian Castaño <ianfe@martina.com.co>
2025-03-12 21:59:39 -0500

    feat: Added guest room get packet

---
+++ room/message/guest_test.go
@@ -0,0 +1,48 @@
+package message
+
+import (
+	"github.com/stretchr/testify/assert"
+	"pixels-emulator/core/protocol"
+	"testing"
+)
+
+// guestPacket is the mocked packet.
+var guestPacket = &GetGuestRoomPacket{
+	RoomId:  1,
+	Enter:   true,
+	Forward: true,
+}
+
+// encodeGuestNavigator generates a decoding helper function to test.
+func encodeGuestNavigator(dec *GetGuestRoomPacket) *protocol.RawPacket {
+	pck := protocol.NewPacket(GetGuestRoomCode)
+	pck.AddInt(dec.RoomId)
+	pck.AddBoolean(dec.Enter)
+	pck.AddBoolean(dec.Forward)
+	return &pck
+}
+
+// TestComposeGuestRoomPacket check if packet is encoded and decoded.
+func TestComposeGuestRoomPacket(t *testing.T) {
+
+	enc := encodeGuestNavigator(guestPacket)
+	bytes := enc.ToBytes()
+
+	newPck, err := protocol.FromBytes(bytes)
+	assert.NoError(t, err, "Protocol byte parsing must not have error")
+
+	dec, err := ComposeGuestRoomPacket(*newPck)
+	assert.NoError(t, err, "Protocol decoding must not have error")
+	assert.Equal(t, guestPacket.RoomId, dec.RoomId)
+	assert.Equal(t, guestPacket.Enter, dec.Enter)
+	assert.Equal(t, guestPacket.Forward, dec.Forward)
+	assert.Equal(t, int(guestPacket.Id()), GetGuestRoomCode)
+
+}
+
+// TestComposeGuestRoomPacketPacketRate verifies that the Rate method returns (0, 0).
+func TestComposeGuestRoomPacketPacketRate(t *testing.T) {
+	mn, mx := guestPacket.Rate()
+	assert.Equal(t, uint16(0), mn, "NavigatorInitPacket.Rate() min mismatch")
+	assert.Equal(t, uint16(0), mx, "NavigatorInitPacket.Rate() max mismatch")
+}
