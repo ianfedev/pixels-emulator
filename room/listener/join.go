6cfd74e3e7fceece4e5629f2df57a1ffe3439db4
Ian Castaño <ianfe@martina.com.co>
2025-03-19 22:09:39 -0500

    feat: Continue looking for context

--- room/listener/join.go
+++ room/listener/join.go
@@ -1,11 +1,13 @@
 package listener
 
 import (
+	"context"
 	"errors"
 	"go.uber.org/zap"
 	"pixels-emulator/core/event"
 	"pixels-emulator/core/server"
 	roomEvent "pixels-emulator/room/event"
+	"time"
 )
 
 // ProvideUserJoin perform user validation when joining a room.
@@ -23,19 +25,36 @@ func OnUserRoomJoin(ev event.Event) {
 	defer func() {
 		if err != nil {
 			server.GetServer().Logger().Error("error during user room join", zap.Error(err))
+			// TODO: Send user to main.
 		}
 	}()
 
-	_, valid := ev.(*roomEvent.RoomJoinEvent)
+	joinEv, valid := ev.(*roomEvent.RoomJoinEvent)
 	if !valid {
-		err = errors.New("event proportioned was not authentication")
+		err = errors.New("event proportioned was not room join")
 		return
 	}
 
+	rs := server.GetServer().RoomStore()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	room, err := rs.Read(ctx, string(joinEv.Id))
+
 	// Check if event is cancelled and send to main
 
-	//// Check if is banned and doesnt have permissions
+	//// Check if is banned and doesnt have permissions (This must differ from normal close)
 
-	// Queue removal
+	// Queue removal: Must find all rooms and remove from queue.
+
+	// Let event proceed
+	// - If event has overriding
+	// - If user is owner
+	// - If user has permissions
+	// - If user has rights
+	// - Guild rights pending on guild system
+	server.GetServer().Logger().Debug("Room", zap.Any("room", room))
+	server.GetServer().Logger().Error("Room", zap.Any("err", err))
 
 }
--- room/store.go
+++ room/store.go
@@ -3,11 +3,11 @@ package room
 import "pixels-emulator/core/store"
 
 type Store struct {
-	store.AsyncStore[Room]
+	store.AsyncStore[*Room]
 }
 
 func NewStore() *Store {
 	return &Store{
-		AsyncStore: store.NewMemoryStore[Room](),
+		AsyncStore: store.NewMemoryStore[*Room](),
 	}
 }
